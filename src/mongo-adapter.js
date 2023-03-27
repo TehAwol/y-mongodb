fa9b468f089f3ab903b68ec89cf45afe6a231c97
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2023-03-27 18:18:18 +0200

    [deps] replace mongojs and mongoist with mongodb

--- src/mongo-adapter.js
+++ src/mongo-adapter.js
@@ -1,48 +1,71 @@
-import mongojs from 'mongojs'
-import mongoist from 'mongoist'
+import { MongoClient } from "mongodb";
 
 export class MongoAdapter {
-  constructor (location, collection) {
-    this.location = location
-    this.collection = collection || 'yjs-writings'
-    this.db = null
-    this.open()
+  /**
+   * @param {string} uri
+   * @param {string} collection
+   */
+  constructor(uri, collection) {
+    this.uri = uri;
+    this.collection = collection;
+    this.db = null;
+    this.client = null;
+    this.connect();
   }
 
-  open () {
-    const mongojsDb = mongojs(this.location, [this.collection])
-    this.db = mongoist(mongojsDb)
+  async connect() {
+    this.client = await MongoClient.connect(this.uri);
+    this.db = this.client.db();
   }
 
-  get (query) {
-    return this.db[this.collection].findOne(query)
+  /**
+   * @param {string} name
+   */
+  getCollection(name) {
+    if (!this.db) {
+      throw new Error("Database not connected");
+    }
+    return this.db.collection(name);
   }
 
-  put (values) {
-    if (!values.docName && !values.version && !values.value) { throw new Error('Document and version must be provided') }
-
-    return this.db[this.collection].save(values)
+  async get(query) {
+    return await this.getCollection(this.collection).findOne(query);
   }
 
-  del (query) {
-    const bulk = this.db[this.collection].initializeOrderedBulkOp()
-    bulk.find(query).remove()
-    return bulk.execute()
+  async put({ docName, version, value }) {
+    if (!docName || !version || !value) {
+      throw new Error("Missing required properties: docName, version, value");
+    }
+    return await this.getCollection(this.collection).updateOne(
+      { docName, version },
+      { $set: { value } },
+      { upsert: true }
+    );
   }
 
-  readAsCursor (query, opts = {}) {
-    let curs = this.db[this.collection].findAsCursor(query)
-    if (opts.reverse) curs = curs.sort({ clock: -1 })
-    if (opts.limit) curs = curs.limit(opts.limit)
-    return curs.toArray()
+  async delete(query) {
+    return await this.getCollection(this.collection).deleteMany(query);
   }
 
-  close () {
-    this.db.close()
+  async readAsCursor(query, { reverse = false, limit = null } = {}) {
+    let cursor = this.getCollection(this.collection).find(query);
+    if (reverse) {
+      cursor = cursor.sort({ clock: -1 });
+    }
+    if (limit) {
+      cursor = cursor.limit(limit);
+    }
+    return await cursor.toArray();
   }
 
-  async flush () {
-    await this.db.dropDatabase()
-    await this.db.close()
+  async close() {
+    if (this.client) await this.client.close();
+  }
+
+  async flush() {
+    if (this.db) {
+      await this.db.dropDatabase();
+      await this.close();
+    }
   }
 }
